06156e9f8162c29ef8671fd4add947158b1d271a
YannSko <yskonieczny@gmail.com>
2024-10-14 19:12:09 +0200

    preprocess

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Sentiments_Analysis
  Little_Application_to_Determine_Sentiment
+
+
+# Data from
+
+https://ai.stanford.edu/~amaas/data/sentiment/
---
+++ preprocess.py
@@ -0,0 +1,27 @@
+# preprocess.py
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+class TextPreprocessor:
+    def __init__(self):
+        self.stop_words = set(stopwords.words("english"))
+        self.lemmatizer = WordNetLemmatizer()
+        
+    def preprocess(self, text):
+        # Tokenization
+        tokens = word_tokenize(text.lower())
+        
+        # Suppression des stopwords
+        tokens = [word for word in tokens if word not in self.stop_words and word.isalpha()]
+        
+        # Lemmatization
+        tokens = [self.lemmatizer.lemmatize(word) for word in tokens]
+        
+        return " ".join(tokens)
