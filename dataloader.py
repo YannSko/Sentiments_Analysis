daf1c3e92ccac148269290928a12819153507880
YannSko <yskonieczny@gmail.com>
2024-10-14 19:23:35 +0200

    dataloader.py

---
+++ dataloader.py
@@ -0,0 +1,18 @@
+# data_loader.py
+
+import pandas as pd
+from preprocess import TextPreprocessor
+
+def load_and_preprocess_data():
+    
+    df = pd.read_csv("https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz", delimiter='\t')
+    
+    # Extraction des données et étiquettes
+    X = df['review']
+    y = df['sentiment']  # 1 = positif, 0 = négatif
+    
+    # Prétraitement des textes
+    preprocessor = TextPreprocessor()
+    X_preprocessed = X.apply(preprocessor.preprocess)
+    
+    return X_preprocessed, y
